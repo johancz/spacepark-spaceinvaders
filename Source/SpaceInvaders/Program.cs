0d11e078e5bbbc56f28d7ab5c7d49dbbb14c2c87
Oskar Karlsson <tilloskar@outlook.com>
2021-03-18 12:22:39 +0100

    Made FetchData, FetchPeople methods Made Person, APIResponse classes

--- Source/SpaceInvaders/Program.cs
+++ Source/SpaceInvaders/Program.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SpaceInvaders
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await FetchPeople();
+        }
+        
+        public static async Task<APIResponse> FetchData(string requestUrl)
+        {
+            string baseUrl = "http://swapi.dev/api/";
+
+            string resource = requestUrl.Substring(baseUrl.Length);
+
+            var client = new RestClient("http://swapi.dev/api/");
+            var request = new RestRequest(resource, DataFormat.Json);
+            // NOTE: The Swreponse is a custom class which represents the data returned by the API, RestClient have buildin ORM which maps the data from the reponse into a given type of object
+            var response = await client.GetAsync<APIResponse>(request);
+            return response;
+        }
+
+        public static async Task FetchPeople()
+        {
+            List<Person> persons = new List<Person>();
+
+            APIResponse response;
+
+            string requestUrl = "http://swapi.dev/api/people/";
+            
+            while (requestUrl != null)
+            {
+                response = await FetchData(requestUrl);
+
+                foreach (var p in response.Results)
+                {
+                    Console.WriteLine(p.Name);
+                    Console.WriteLine(p.Homeworld);
+
+                    foreach (var s in p.Starships)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+
+                requestUrl = response.Next;
+            }
         }
     }
 }
