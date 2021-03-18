c339724aeac7f9f6b4eecd930debf912d4e2d286
Orhan Albayati <70097003+Orhan92@users.noreply.github.com>
2021-03-18 13:45:01 +0100

    Cleaned up comments

--- Source/SpaceInvaders/Traveller/Person.cs
+++ Source/SpaceInvaders/Traveller/Person.cs
@@ -10,14 +10,6 @@ namespace SpaceInvaders.Traveller
     public class Person
     {
         public string Name { get; set; }
-        //public string Homeworld { get; set; }
         public List<string> Starships { get; set; }
-        //public List<Traveller> travellers { get; set; }
-
-        //public People(string name, string planet)
-        //{
-        //    this.Name = name;
-        //    this.Planet = planet;
-        //}
     }
 }
