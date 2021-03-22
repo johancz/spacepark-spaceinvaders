a9301cf8035443d7f770c0db0e8ee5ed9557585a
JoCh <johancz@users.noreply.github.com>
2021-03-22 17:42:00 +0100

    Add private field for the parking length limit and reduce pyramid code

--- Source/SpaceInvaders/Program.cs
+++ Source/SpaceInvaders/Program.cs
@@ -15,6 +15,8 @@ namespace SpaceInvaders
 {
     class Program
     {
+        private const double _parkingLengthLimit = 35;
+
         static async Task Main(string[] args)
         {
             //Added this line to Parse double values to not mix "." and ","
@@ -38,85 +40,85 @@ namespace SpaceInvaders
                 if (selectedMenu == 0)
                 {
                     Console.WriteLine("Who are you traveller? ");
-
                     var peopleList = await Fetch.People(Console.ReadLine());
-                    
                     Console.WriteLine();
 
+                    // If the person is not a Star Wars character, go back to the start menu
                     if (peopleList.Count == 0)
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Sorry, you are not a Starwars character. Back to the void with ya!");
                         Console.ForegroundColor = ConsoleColor.White;
+                        continue; // Go back to the start menu.
                     }
-                    else
+
+                    int selectedMenuPerson = 0;
+                    if (peopleList.Count > 1)
                     {
-                        int selectedMenuPerson = 0;
-                        if (peopleList.Count > 1)
-                        {
-                            selectedMenuPerson = Menu.Options("Please select ", peopleList.Select(p => p.Name).ToArray());
-                        }
+                        selectedMenuPerson = Menu.Options("Please select ", peopleList.Select(p => p.Name).ToArray());
+                    }
 
-                        Person selectedPerson = peopleList[selectedMenuPerson];
+                    Person selectedPerson = peopleList[selectedMenuPerson];
 
-                        if (DatabaseQueries.CheckParking(selectedPerson.Name) != null)
-                        {
-                            Console.WriteLine("You have already parked\nPress any key to continue");
-                            Console.ReadKey();
-                            Console.Clear();
-                            continue;
-                        }
-
-                        // Fetch all Starships
-                        var allStarships = await Fetch.Starships();
+                    // If the person is already parked, go back to the start menu
+                    if (DatabaseQueries.CheckParking(selectedPerson.Name) != null)
+                    {
+                        Console.WriteLine("You have already parked\nPress any key to continue");
+                        Console.ReadKey();
                         Console.Clear();
-                        List<Starships> personalShips = allStarships.Join(selectedPerson.Starships,
-                                                                      s1 => s1.URL, s2 => s2,
-                                                                      (s1, s2) => s1).ToList();
+                        continue; // Go back to the start menu.
+                    }
 
-                        //If the character doesn't own a starship
-                        if (personalShips.Count == 0)
+                    // Fetch all Starships
+                    var allStarships = await Fetch.Starships();
+                    Console.Clear();
+                    List<Starships> personalShips = allStarships.Join(selectedPerson.Starships,
+                                                                    s1 => s1.URL, s2 => s2,
+                                                                    (s1, s2) => s1).ToList();
+
+                    // If the character doesn't own a starship, go back to the start menu
+                    if (personalShips.Count == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"There is no registered starship under the name of {selectedPerson.Name}.");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        continue; // Go back to the start menu.
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Welcome, {selectedPerson.Name}!\n");
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    int selectedShipIndex = Menu.Options("Please select your ship", personalShips.Select(p => p.Name).ToArray());
+                    Starships selectedShip = personalShips[selectedShipIndex];
+                    Console.WriteLine("\nLoading...");
+                    Thread.Sleep(2000);
+                    Console.Clear();
+
+                    // todo: SLÄNG IN NEDAN I FUNKTION
+                    // We check if the starship fits in the parkinglot
+                    // Parsing because the Length attribute is a String type
+                    if (double.TryParse(selectedShip.Length, out double result))
+                    {
+                        if (result <= _parkingLengthLimit)
                         {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine($"There is no registered starship under the name of {selectedPerson.Name}.");
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine($"You selected: {selectedShip.Name}, Length: {selectedShip.Length}m");
                             Console.ForegroundColor = ConsoleColor.White;
+
+                            //Add parking into database
+                            DatabaseQueries.AddParking(selectedPerson, selectedShip);
                         }
                         else
                         {
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.WriteLine($"Welcome, {selectedPerson.Name}!\n");
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"We're sorry but your {selectedShip.Name} is too big ({selectedShip.Length}m) for our parking lots. (Maximum length: 30m)");
                             Console.ForegroundColor = ConsoleColor.White;
-
-                            int selectedShipIndex = Menu.Options("Please select your ship", personalShips.Select(p => p.Name).ToArray());
-                            Starships selectedShip = personalShips[selectedShipIndex];
-                            Console.WriteLine("\nLoading...");
-                            Thread.Sleep(2000);
-                            Console.Clear();
-
-                            //SLÄNG IN NEDAN I FUNKTION
-                            //We check if the starship fits in the parkinglot
-                            //Parsing because the Length attribute is a String type
-                            if (double.TryParse(selectedShip.Length, out double result))
-                            {
-                                if (result <= 35)
-                                {
-                                    Console.ForegroundColor = ConsoleColor.Yellow;
-                                    Console.WriteLine($"You selected: {selectedShip.Name}, Length: {selectedShip.Length}m");
-                                    Console.ForegroundColor = ConsoleColor.White;
-
-                                    //Add parking into database
-                                    DatabaseQueries.AddParking(selectedPerson, selectedShip);
-                                }
-                                else
-                                {
-                                    Console.ForegroundColor = ConsoleColor.Red;
-                                    Console.WriteLine($"We're sorry but your {selectedShip.Name} is too big ({selectedShip.Length}m) for our parking lots. (Maximum length: 30m)");
-                                    Console.ForegroundColor = ConsoleColor.White;
-                                    //Do nothing as we cannot park the ship
-                                }
-                            }
+                            //Do nothing as we cannot park the ship
                         }
                     }
+                    // todo: felhantering om TryParse == false?
+
                     Console.WriteLine();
                 }
                 else if (selectedMenu == 1)
@@ -132,6 +134,6 @@ namespace SpaceInvaders
                     break;
                 }
             }
-        }      
+        }
     }
 }
